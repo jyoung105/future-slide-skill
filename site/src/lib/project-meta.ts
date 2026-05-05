08d9c219d96c7bf061e1b2319718902c0f09397f
jyoung105 <jeongmin1604@gmail.com>
2026-05-05 21:49:13 +0900

    refactor(site): Remove React Project Metadata

--- site/src/lib/project-meta.ts
+++
@@ -1,8 +0,0 @@
-export const PROJECT_META = {
-  authorName: "Tony",
-  authorUrl: "https://tonylee.im",
-  githubUrl: "https://github.com/jyoung105/future-slide-skill",
-  license: "Apache-2.0",
-  version: "0.0.2",
-  copyrightYear: "2026",
-} as const;
