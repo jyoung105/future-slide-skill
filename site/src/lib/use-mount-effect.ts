c7b10049f71504a5d7cd0b89918daccbd083a6cb
jyoung105 <jeongmin1604@gmail.com>
2026-05-05 21:49:13 +0900

    refactor(site): Remove Mount Effect Hook

--- site/src/lib/use-mount-effect.ts
+++
@@ -1,6 +0,0 @@
-import { useEffect } from "react";
-
-export function useMountEffect(effect: () => void | (() => void)) {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(effect, []);
-}
